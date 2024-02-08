7a94d2f7d8b974a54f8d10eb66c9629475e87294
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-08 12:24:24 -0500

    Se modificaron los script y los daños que tenian los objetos

---
+++ Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+   [SerializeField, Range(-60,50), Tooltip("limite izquierdo")]  private float leftLimit = -30;
+    [ SerializeField, Range(-1,-15), Tooltip("limite hacia abajo ")]  float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+            Debug.Log("Eliminado objeto" + gameObject.name);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+      else if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+            //Time.timeScale = 0.1f;
+            Debug.Log("Game Over");
+        }
+
+    }
+}
---
+++ Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    private int indexBall;
+
+    [SerializeField, Range(-21, 8)] float spawnLimitXLeft = -21;
+    [SerializeField, Range(8, -20)] float spawnLimitXRight = 8;
+    private float spawnPosY = 28;
+
+    [SerializeField, Range(0.1f, 5)] float startDelay = 1.0f;
+    [SerializeField, Range(0.1f, 5)] float spawnInterval = 2.0f;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        indexBall = Random.Range(0, ballPrefabs.Length);
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[indexBall], spawnPos, ballPrefabs[indexBall].transform.rotation);
+    }
+
+}
