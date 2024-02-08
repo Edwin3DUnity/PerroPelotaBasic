df4ef12e28e9fb3aaf0c8eadba75a7ff8a382b9e
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-08 14:16:25 -0500

    Se modificaron los scripts playerController y spawnManager con un conteo para spawnear y disparar

--- Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,39 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float contador = 0;
+
+    private float esperarParaNuevoDisparo = 1;
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+
+
+
+        contador += Time.deltaTime;
+
+        if (contador >= esperarParaNuevoDisparo)
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+                contador = 0;
+                esperarParaNuevoDisparo = Random.Range(1, 3);
+
+
+            }
+
         }
-    }
+            
+           
+            
+            
+            
+    
+    
+    
+        }
+       
 }
--- Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManagerX : MonoBehaviour
 {
@@ -12,14 +14,21 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 28;
 
     [SerializeField, Range(0.1f, 5)] float startDelay = 1.0f;
-    [SerializeField, Range(0.1f, 5)] float spawnInterval = 2.0f;
+    [SerializeField, Range(0.1f, 5)] float spawnInterval = 5;
 
-    
-    
+
+    //private float counter = 0;
+
+  //  private float nextWaitTime = 5;
     // Start is called before the first frame update
     void Start()
     {
-      //  InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+       InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    private void Update()
+    {
+     //   SpawnRandomBall();
     }
 
     // Spawn random ball at random x position at top of play area
@@ -29,8 +38,19 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
         Instantiate(ballPrefabs[indexBall], spawnPos, ballPrefabs[indexBall].transform.rotation);
+     /*  counter += Time.deltaTime;
+        if (counter >= nextWaitTime)
+        {
+            Instantiate(ballPrefabs[indexBall], spawnPos, ballPrefabs[indexBall].transform.rotation);
+            counter = 0;
+            nextWaitTime = Random.Range(2, 5);
+            Debug.Log(nextWaitTime);
+        }
+        // instantiate ball at random spawn location
+     */
+
+     spawnInterval = Random.Range(2, 5);
     }
 
 }
